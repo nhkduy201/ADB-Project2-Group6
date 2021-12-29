b1ff6f47fb0587f79c574cfdd0883fea9f4ca9b4
LinhTran2012 <tranlinh981@gmail.com>
2021-12-30 00:03:36 +0700

    Update Table+Keys.sql

--- DB/Table+Keys.sql
+++ DB/Table+Keys.sql
@@ -73,6 +73,7 @@ create table HOADON
 	TyLeGiamGia float,
 	LoaiHoaDon bit,
 	HinhThucThanhToan nvarchar(20), --(Tiền mặt,Online)
+	TongTien int,
 	MaKhachHang int,
 	MaNhanVien int,
 	constraint PK_HD primary key(MaHoaDon)
