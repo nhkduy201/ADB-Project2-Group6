3e4e3a6f68295805f655e3709783b6abfd264fbc
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2022-01-01 00:27:33 +0700

    insert, update, delete product

--- models/product.models.js
+++ models/product.models.js
@@ -22,13 +22,19 @@ export default {
 
     async findProductByTypeId(id, limit, offset){
         await sql.connect(sqlConfig);
-        const obj = await sql.query`select * from SANPHAM where MaLoai = ${id} order by MaSanPham ASC offset ${offset} rows fetch next ${limit} rows only`;
+        const obj = await sql.query`select * from SANPHAM where MaLoai = ${id} and TinhTrang = 1 order by MaSanPham ASC offset ${offset} rows fetch next ${limit} rows only`;
+        return obj;
+    },
+
+    async findProductById(id){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`select * from SANPHAM where MaSanPham = ${id}`;
         return obj;
     },
 
     async findAllProducts(limit, offset){
         await sql.connect(sqlConfig);
-        const obj = await sql.query`select * from SANPHAM order by MaSanPham ASC offset ${offset} rows fetch next ${limit} rows only`;
+        const obj = await sql.query`select * from SANPHAM where TinhTrang = 1 order by MaSanPham  ASC offset ${offset} rows fetch next ${limit} rows only`;
         return obj;
     },
 
@@ -37,4 +43,46 @@ export default {
         const obj = await sql.query`select * from LOAISANPHAM where MaLoaiCapCha = ${parentType}`;
         return obj;
     },
+
+    async getAllProductType(){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`select * from LOAISANPHAM`;
+        return obj;
+    },
+
+    async getAllSupplier(){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`select * from NHACUNGCAP`;
+        return obj;
+    },
+
+    async getAllProductID(){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`select MaSanPham from SANPHAM`;
+        return obj;
+    },
+
+    async addProduct(id, name, type, supplier, country, size, count, des, price){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`insert into SanPham values (${id}, ${type}, ${supplier}, ${name}, 0, ${country}, ${size}, 
+                            ${count}, ${des}, 1, 1)`;
+        var d = new Date();
+        d.setHours(d.getHours() + 7);
+        const obj2 = await sql.query`insert into LichSuGia values (${id}, ${d}, ${price})`;
+        return obj;
+    },
+
+    async delProduct(id){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`update SANPHAM set TinhTrang = 0 where MaSanPham = ${id}`;
+        return obj;
+    },
+
+    async updateProduct(id, name, type, supplier, country, size, count, des){
+        await sql.connect(sqlConfig);
+        const obj = await sql.query`update SANPHAM set TenSanPham = ${name}, MaLoai = ${type},
+                    MaNhaCungCap = ${supplier}, QuocGiaSanXuat = ${country}, KichThuoc = ${size},
+                    DonViDoLuong = ${count}, MoTaChiTiet = ${des} where MaSanPham = ${id}`;
+        return obj;
+    }
 };
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from 'morgan';
 import viewMdw from "./middlewares/view.mdw.js";
 import localMdw from "./middlewares/local.mdw.js";
-import productModels from "./models/product.models.js";
 import AuthModels from "./models/auth.models.js";
 import ProductModels from "./models/product.models.js";
 
@@ -24,13 +23,94 @@ app.get("/register", function (req, res){
     res.render("register");
 });
 
+app.get('/admin/products/add', async function(req, res){
+    const obj1 = await ProductModels.getAllProductType();
+    const typeList = obj1.recordset;
+    const obj2 = await ProductModels.getAllSupplier();
+    const supList = obj2.recordset;
+    res.render('add', {
+        layout: 'bs4.hbs',
+        typeList,
+        supList
+    });
+});
+
+app.post('/admin/products/add', async function(req, res){
+    const name = req.body.ProName;
+    const obj1 = req.body.ProType;
+    const obj2 = req.body.ProSup;
+    const type = obj1.split(" - ")[0];
+    const supplier = obj2.split(" - ")[0];
+    const country = req.body.ProCountry;
+    const size = req.body.ProSize;
+    const count = req.body.ProCount;
+    const des = req.body.ProDes;
+    const price = req.body.ProPrice;
+    const obj = await ProductModels.getAllProductID();
+    const idList = obj.recordset;
+    var id = idList.length + 1;
+    const ret = ProductModels.addProduct(id, name, type, supplier, country, size, count, des, price);
+    console.log(id)
+    res.redirect("/admin/products/1");
+});
+
+app.get("/admin/products/edit", async function (req, res){
+    const id = req.query.id;
+    const obj = await ProductModels.findProductById(id);
+    const product = obj.recordset[0];
+    const obj1 = await ProductModels.getAllProductType();
+    const typeList = obj1.recordset;
+    const obj2 = await ProductModels.getAllSupplier();
+    const supList = obj2.recordset;
+    for (var i = 0; i < typeList.length; i++){
+        if (typeList[i].MaLoai === product.MaLoai){
+            product.TenLoai = typeList[i].TenLoai;
+            break;
+        }
+    }
+    for (var i = 0; i < supList.length; i++){
+        if (supList[i].MaNhaCungCap === product.MaNhaCungCap){
+            product.TenNhaCungCap = supList[i].TenNhaCungCap;
+            break;
+        }
+    }
+    console.log(product);
+    res.render('edit', {
+        layout: 'bs4.hbs',
+        product,
+        typeList,
+        supList
+    });
+});
+
+app.post("/admin/products/del", async function(req, res){
+    const id = req.body.ProID;
+    const ret = ProductModels.delProduct(id);
+    res.redirect("/admin/products/1");
+});
+
+app.post("/admin/products/patch", async function (req, res){
+    const id = req.body.ProID;
+    const name = req.body.ProName;
+    const obj1 = req.body.ProType;
+    const obj2 = req.body.ProSup;
+    const type = obj1.split(" - ")[0];
+    const supplier = obj2.split(" - ")[0];
+    const country = req.body.ProCountry;
+    const size = req.body.ProSize;
+    const count = req.body.ProCount;
+    const des = req.body.ProDes;
+    const ret = ProductModels.updateProduct(id, name, type, supplier, country, size, count, des);
+    res.redirect("/admin/products/1");
+});
+
 app.get("/admin/products/:page", async function (req, res){
     var page = req.params.page || 1;
     page = parseInt(page);
     if (page < 1)
         page = 1;
     const offset = (page-1)*10;
-    const obj = await productModels.findAllProducts(10, offset);
+    const obj = await ProductModels.findAllProducts(10, offset);
     const products = obj.recordset;
     res.render("admin", {
         layout: "bs4.hbs",
@@ -95,7 +175,7 @@ app.get("/products/bycat", async function (req, res){
     if (page < 1)
         page = 1;
     const offset = (page-1)*10;
-    const obj = await productModels.findProductByTypeId(type, 10, offset);
+    const obj = await ProductModels.findProductByTypeId(type, 10, offset);
     const products = obj.recordset;
     res.render("byCat", {
         layout: "bs4.hbs",
