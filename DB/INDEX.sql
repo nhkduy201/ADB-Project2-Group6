a018cac3f35605da92025c8a1e02771c4b9af705
Nguyễn Thị Như Phương <92770267+nhuphuongs@users.noreply.github.com>
2022-01-02 21:16:01 +0700

    Update INDEX.sql

--- DB/INDEX.sql
+++ DB/INDEX.sql
@@ -13,3 +13,7 @@ CREATE NONCLUSTERED INDEX MaNhanVien_idx ON HOADON (MaNhanVien)
 CREATE NONCLUSTERED INDEX TenSanPham_idx ON SANPHAM (TenSanPham)
 --Truy vấn 3:
 CREATE NONCLUSTERED INDEX MaLoai_MaNhaCungCap_idx ON SANPHAM (MaLoai,MaNhaCungCap)
+
+--Phân hệ QUẢN LÝ:
+--Truy vấn 2:
+create nonclustered index MaNhanVien_idx on HOADON(MaNhanVien)
